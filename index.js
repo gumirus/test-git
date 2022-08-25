34f888faeee9902011a4ebc56963cff678ce9e3a
Ruslan <41588642+gumirus@users.noreply.github.com>
2022-08-25 15:05:29 +0500

    lession import export

---
+++ index.js
@@ -0,0 +1,7 @@
+import './index.css';
+// import { App as AppComponent, createFunc } from './src/modules/app'
+import App  from './src/modules/app';
+const app = new App();
+app.run();
+
+console.log('__work__');
